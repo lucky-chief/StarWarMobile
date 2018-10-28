ac0b34893df0aeab8728c79f956ad3601af026db
jianjun.wu <371860067@qq.com>
2018-10-28 16:07:53 +0800

    first commit by jianjun

---
+++ Assets/TableDatabase/Editor/Data/Table/BaseAttributeTableConfig.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseAttributeTableConfig : ScriptableObject
+{
+    public List<float> ColumnsWidth = new List<float>();
+    public int ShowCount = 10;
+}
