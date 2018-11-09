982cf3cc28169985aa5fd0bde159f803ae981736
lucky-chief <278830890@qq.com>
2018-11-09 09:37:45 +0800

    框架代码开发

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+	public enum Layers
+	{
+		BuildingLayer,
+		EnermyGroundLayer,
+		SelfGroundLayer,
+		EnermySkyLayer,
+		SelfSkyLayer,
+		BulletLayer
+	}
+
+	public void InitLayers(List<Layers> layers)
+	{
+
+	}
+
+	public void LoadMapBackground(string path)
+	{
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
